e6bcf84540e7e471af641718fb3ab4389c96f282
Robert Jäschke <jaeschke@l3s.de>
2015-04-03 15:20:44 +0200

    add fullscreen and keyboard support

--- hdaps-gl.c
+++ hdaps-gl.c
@@ -38,6 +38,9 @@ static int val_y;
 static int rest_x;
 static int rest_y;
 
+static int fullscreen = 1;
+static int window;
+
 /*
  * read_position - read the (x,y) position pair from hdaps.
  *
@@ -52,218 +55,248 @@ static int rest_y;
  */
 static int read_position (int *x, int *y)
 {
-	char buf[BUF_LEN];
-	int fd, ret;
+  char buf[BUF_LEN];
+  int fd, ret;
 
-	fd = open (SYSFS_POSITION_FILE, O_RDONLY);
-	if (fd < 0) {
-		perror ("open");
-		return fd;
-	}	
+  fd = open (SYSFS_POSITION_FILE, O_RDONLY);
+  if (fd < 0) {
+    perror ("open");
+    return fd;
+  }
 
-	ret = read (fd, buf, BUF_LEN);
-	if (ret < 0) {
-		perror ("read");
-		goto out;
-	} else if (ret == 0) {
-		fprintf (stderr, "error: unexpectedly read zero!\n");
-		ret = 1;
-		goto out;
-	}
-	ret = 0;
+  ret = read (fd, buf, BUF_LEN);
+  if (ret < 0) {
+    perror ("read");
+    goto out;
+  } else if (ret == 0) {
+    fprintf (stderr, "error: unexpectedly read zero!\n");
+    ret = 1;
+    goto out;
+  }
+  ret = 0;
 
-	if (sscanf (buf, "(%d,%d)\n", x, y) != 2)
-		ret = 1;
+  if (sscanf (buf, "(%d,%d)\n", x, y) != 2)
+    ret = 1;
 
-out:
-	if (close (fd))
-		perror ("close");
+ out:
+  if (close (fd))
+    perror ("close");
 
-	return ret;
+  return ret;
 }
 
 static void resize_scene (int width, int height)
 {
-	if (height == 0)
-		height = 1;
-	glViewport (0, 0, width, height);
-	glMatrixMode (GL_PROJECTION);
-	glLoadIdentity ();
-	gluPerspective (45.0f, (GLfloat)width / (GLfloat)height, 0.1f, 100.0f);
-	glMatrixMode (GL_MODELVIEW);
+  if (height == 0)
+    height = 1;
+  glViewport (0, 0, width, height);
+  glMatrixMode (GL_PROJECTION);
+  glLoadIdentity ();
+  gluPerspective (45.0f, (GLfloat)width / (GLfloat)height, 0.1f, 100.0f);
+  glMatrixMode (GL_MODELVIEW);
 }
 
 static void draw_scene (void)
 {
-	glClear (GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
-	glPushMatrix ();
+  glClear (GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+  glPushMatrix ();
 
-	glRotated (val_x / 2.0, 0.0f, 0.0f, -1.0f);
-	glRotated (val_y / 2.0, 1.0f, 0.0f, 0.0f);
+  glRotated (val_x / 2.0, 0.0f, 0.0f, -1.0f);
+  glRotated (val_y / 2.0, 1.0f, 0.0f, 0.0f);
 
-	glBegin (GL_QUADS);	// start drawing the laptop.
+  glBegin (GL_QUADS);	// start drawing the laptop.
 
-	// top of body
-	glColor3d (0.0f, 0.0f, 0.0f);
-	glVertex3d (1.0f, 0.0f, 1.6f);
-	glVertex3d (-1.0f, 0.0f, 1.6f);
-	glVertex3d (-1.0f, 0.0f, 0.0f);
-	glVertex3d (1.0f, 0.0f, 0.0f);
+  // top of body
+  glColor3d (0.0f, 0.0f, 0.0f);
+  glVertex3d (1.0f, 0.0f, 1.6f);
+  glVertex3d (-1.0f, 0.0f, 1.6f);
+  glVertex3d (-1.0f, 0.0f, 0.0f);
+  glVertex3d (1.0f, 0.0f, 0.0f);
 
-	// trackpoint
-	glColor3d (1.0f, 0.0f, 0.0f);
-	glVertex3d (0.01f, 0.01f, 0.78f);
-	glVertex3d (0.01f, 0.01f, 0.82f);
-	glVertex3d (-0.01f, 0.01f, 0.82f);
-	glVertex3d (-0.01f, 0.01f, 0.78f);
+  // trackpoint
+  glColor3d (1.0f, 0.0f, 0.0f);
+  glVertex3d (0.01f, 0.01f, 0.78f);
+  glVertex3d (0.01f, 0.01f, 0.82f);
+  glVertex3d (-0.01f, 0.01f, 0.82f);
+  glVertex3d (-0.01f, 0.01f, 0.78f);
 
-	// bottom of body
-	glColor3d (0.0f, 0.0f, 0.0f);
-	glVertex3d (1.0f, -0.1f, -0.1f);
-	glVertex3d (-1.0f, -0.1f, -0.1f);
-	glVertex3d (-1.0f, -0.1f, 1.6f);
-	glVertex3d (1.0f, -0.1f, 1.6f);
+  // bottom of body
+  glColor3d (0.0f, 0.0f, 0.0f);
+  glVertex3d (1.0f, -0.1f, -0.1f);
+  glVertex3d (-1.0f, -0.1f, -0.1f);
+  glVertex3d (-1.0f, -0.1f, 1.6f);
+  glVertex3d (1.0f, -0.1f, 1.6f);
 
-	// front of body
-	glColor3d (0.0f, 0.0f, 0.0f);
-	glVertex3d (1.0f, 0.0f, 1.6f);
-	glVertex3d (-1.0f, 0.0f, 1.6f);
-	glVertex3d (-1.0f, -0.1f, 1.6f);
-	glVertex3d (1.0f, -0.1f, 1.6f);
+  // front of body
+  glColor3d (0.0f, 0.0f, 0.0f);
+  glVertex3d (1.0f, 0.0f, 1.6f);
+  glVertex3d (-1.0f, 0.0f, 1.6f);
+  glVertex3d (-1.0f, -0.1f, 1.6f);
+  glVertex3d (1.0f, -0.1f, 1.6f);
 
-	// left of body
-	glColor3d (0.0f, 0.0f, 0.0f);
-	glVertex3d (-1.0f, 0.0f, 1.6f);
-	glVertex3d (-1.0f, 0.0f, 0.0f);
-	glVertex3d (-1.0f, -0.1f, 0.0f);
-	glVertex3d (-1.0f, -0.1f, 1.6f);
+  // left of body
+  glColor3d (0.0f, 0.0f, 0.0f);
+  glVertex3d (-1.0f, 0.0f, 1.6f);
+  glVertex3d (-1.0f, 0.0f, 0.0f);
+  glVertex3d (-1.0f, -0.1f, 0.0f);
+  glVertex3d (-1.0f, -0.1f, 1.6f);
 
-	// Right of body
-	glColor3d (0.0f, 0.0f, 0.0f);
-	glVertex3d (1.0f, 0.0f, 0.0f);
-	glVertex3d (1.0f, 0.0f, 1.6f);
-	glVertex3d (1.0f, -0.1f, 1.6f);
-	glVertex3d (1.0f, -0.1f, 0.0f);
+  // Right of body
+  glColor3d (0.0f, 0.0f, 0.0f);
+  glVertex3d (1.0f, 0.0f, 0.0f);
+  glVertex3d (1.0f, 0.0f, 1.6f);
+  glVertex3d (1.0f, -0.1f, 1.6f);
+  glVertex3d (1.0f, -0.1f, 0.0f);
 
-	// top of screen
-	glColor3d (0.0f, 0.0f, 0.0f);
-	glVertex3d (1.0f, 1.6f, -0.1f);
-	glVertex3d (-1.0f, 1.6f, -0.1f);
-	glVertex3d (-1.0f, 1.6f, 0.0f);
-	glVertex3d (1.0f, 1.6f, 0.0f);
+  // top of screen
+  glColor3d (0.0f, 0.0f, 0.0f);
+  glVertex3d (1.0f, 1.6f, -0.1f);
+  glVertex3d (-1.0f, 1.6f, -0.1f);
+  glVertex3d (-1.0f, 1.6f, 0.0f);
+  glVertex3d (1.0f, 1.6f, 0.0f);
 
-	// front of screen
-	glColor3d (0.0f, 0.0f, 0.0f);
-	glVertex3d (1.0f, 1.6f, 0.0f);
-	glVertex3d (-1.0f, 1.6f, 0.0f);
-	glVertex3d (-1.0f, 0.0f, 0.0f);
-	glVertex3d (1.0f, 0.0f, 0.0f);
+  // front of screen
+  glColor3d (0.0f, 0.0f, 0.0f);
+  glVertex3d (1.0f, 1.6f, 0.0f);
+  glVertex3d (-1.0f, 1.6f, 0.0f);
+  glVertex3d (-1.0f, 0.0f, 0.0f);
+  glVertex3d (1.0f, 0.0f, 0.0f);
 
-	// screen
-	glColor3d (0.0f, 0.0f, 1.0f);
-	glVertex3d (0.9f, 1.5f, 0.01f);
-	glVertex3d (-0.9f, 1.5f, 0.01f);
-	glVertex3d (-0.9f, 0.1f, 0.01f);
-	glVertex3d (0.9f, 0.1f, 0.01f);
+  // screen
+  glColor3d (0.0f, 0.0f, 1.0f);
+  glVertex3d (0.9f, 1.5f, 0.01f);
+  glVertex3d (-0.9f, 1.5f, 0.01f);
+  glVertex3d (-0.9f, 0.1f, 0.01f);
+  glVertex3d (0.9f, 0.1f, 0.01f);
 
-	// back of screen
-	glColor3d (0.0f, 0.0f, 0.0f);
-	glVertex3d (1.0f, -0.1f, -0.1f);
-	glVertex3d (-1.0f, -0.1f, -0.1f);
-	glVertex3d (-1.0f, 1.6f, -0.1f);
-	glVertex3d (1.0f, 1.6f, -0.1f);
+  // back of screen
+  glColor3d (0.0f, 0.0f, 0.0f);
+  glVertex3d (1.0f, -0.1f, -0.1f);
+  glVertex3d (-1.0f, -0.1f, -0.1f);
+  glVertex3d (-1.0f, 1.6f, -0.1f);
+  glVertex3d (1.0f, 1.6f, -0.1f);
 
-	// left of screen
-	glColor3d (0.0f, 0.0f, 0.0f);
-	glVertex3d (-1.0f, 1.6f, 0.0f);
-	glVertex3d (-1.0f, 1.6f, -0.1f);
-	glVertex3d (-1.0f, -0.1f, -0.1f);
-	glVertex3d (-1.0f, -0.1f, 0.0f);
+  // left of screen
+  glColor3d (0.0f, 0.0f, 0.0f);
+  glVertex3d (-1.0f, 1.6f, 0.0f);
+  glVertex3d (-1.0f, 1.6f, -0.1f);
+  glVertex3d (-1.0f, -0.1f, -0.1f);
+  glVertex3d (-1.0f, -0.1f, 0.0f);
 
-	// Right of screen
-	glColor3d (0.0f, 0.0f, 0.0f);
-	glVertex3d (1.0f, 1.6f, -0.1f);
-	glVertex3d (1.0f, 1.6f, 0.0f);
-	glVertex3d (1.0f, -0.1f, 0.0f);
-	glVertex3d (1.0f, -0.1f, -0.1f);
+  // Right of screen
+  glColor3d (0.0f, 0.0f, 0.0f);
+  glVertex3d (1.0f, 1.6f, -0.1f);
+  glVertex3d (1.0f, 1.6f, 0.0f);
+  glVertex3d (1.0f, -0.1f, 0.0f);
+  glVertex3d (1.0f, -0.1f, -0.1f);
 
-	glEnd ();
-	glPopMatrix ();
-	glutSwapBuffers ();
+  glEnd ();
+  glPopMatrix ();
+  glutSwapBuffers ();
 }
 
 static void update_scene (void)
 {
-	int ret, x, y, do_update = 0;
+  int ret, x, y, do_update = 0;
 
-	ret = read_position (&x, &y);
-	if (ret)
-		exit(EXIT_FAILURE);
+  ret = read_position (&x, &y);
+  if (ret)
+    exit(EXIT_FAILURE);
 
-	x -= rest_x;
-	y -= rest_y;
+  x -= rest_x;
+  y -= rest_y;
 
-	/* only update if we surpass our threshold, to minimize jitter ... */ 
-	if (abs (x - val_x) > UPDATE_THRESHOLD) {
-		val_x = x;
-		do_update = 1;
-	}
-	if (abs (y - val_y) > UPDATE_THRESHOLD) {
-		val_y = y;
-		do_update = 1;
-	}
+  /* only update if we surpass our threshold, to minimize jitter ... */
+  if (abs (x - val_x) > UPDATE_THRESHOLD) {
+    val_x = x;
+    do_update = 1;
+  }
+  if (abs (y - val_y) > UPDATE_THRESHOLD) {
+    val_y = y;
+    do_update = 1;
+  }
 
-	/* ... or, if we are within our threshold of zero, reset to zero */
-	if (abs (x) < UPDATE_THRESHOLD) {
-		val_x = 0;
-		do_update = 1;
-	}
-	if (abs (y) < UPDATE_THRESHOLD) {
-		val_y = 0;
-		do_update = 1;
-	}
+  /* ... or, if we are within our threshold of zero, reset to zero */
+  if (abs (x) < UPDATE_THRESHOLD) {
+    val_x = 0;
+    do_update = 1;
+  }
+  if (abs (y) < UPDATE_THRESHOLD) {
+    val_y = 0;
+    do_update = 1;
+  }
 
-	if (do_update)
-		draw_scene ();
+  if (do_update)
+    draw_scene ();
 
-	usleep (SLEEP_INTERVAL);
+  usleep (SLEEP_INTERVAL);
+}
+
+static void handle_keyboard (unsigned char key, int x, int y)
+{
+  switch (key)
+    {
+    case 'f': // exit from full screen
+      if (fullscreen == 1) {
+	glutPositionWindow (0,0);
+	glutReshapeWindow (WIDTH, HEIGHT);
+	fullscreen = 0;
+      } else {
+	glutFullScreen ();
+	fullscreen = 1;
+      }
+      break;
+    case 'q': // exit
+      glutDestroyWindow (window);
+      exit (0);
+    case 27: // ESC key - exit
+      glutDestroyWindow (window);
+      exit (0);
+      break;
+    }
+  glutPostRedisplay();
 }
 
 int main (int argc, char *argv[])
 {
-	int ret;
+  int ret;
 
-	ret = read_position (&rest_x, &rest_y);
-	if (ret)
-		return 1;
+  ret = read_position (&rest_x, &rest_y);
+  if (ret)
+    return 1;
 
-	glutInit (&argc, argv);
-	glutInitDisplayMode (GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
-	glutInitWindowSize (WIDTH, HEIGHT);
-	glutInitWindowPosition (0, 0);
-	glutCreateWindow ("IBM Accelerometer Demo");
-	glutDisplayFunc (&update_scene);
-	glutIdleFunc (&update_scene);
-	glutReshapeFunc (&resize_scene);
+  glutInit (&argc, argv);
+  glutInitDisplayMode (GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
 
-	glClearColor (0.5f, 0.5f, 0.5f, 0.0f);
-	glClearDepth (1.0);	// Enables Clearing Of The Depth Buffer
-	glDepthFunc (GL_LESS);	// The Type Of Depth Test To Do
-	glEnable (GL_DEPTH_TEST);	// Enables Depth Testing
-	glShadeModel (GL_SMOOTH);	// Enables Smooth Color Shading
+  glutInitWindowSize (WIDTH, HEIGHT);
+  glutInitWindowPosition (0, 0);
+  window = glutCreateWindow ("IBM Accelerometer Demo");
 
-	glMatrixMode (GL_PROJECTION);
-	glLoadIdentity ();
-	gluPerspective (45.0f, (GLfloat)WIDTH / (GLfloat)HEIGHT, 0.1f, 100.0f);
+  glutFullScreen();
+  glutKeyboardFunc(&handle_keyboard);
 
-	glMatrixMode (GL_MODELVIEW);
-	glLoadIdentity ();
-	glTranslated (0.0, -0.5, -4.0);
+  glutDisplayFunc (&update_scene);
+  glutIdleFunc (&update_scene);
+  glutReshapeFunc (&resize_scene);
 
-	draw_scene ();
+  glClearColor (0.5f, 0.5f, 0.5f, 0.0f);
+  glClearDepth (1.0);	// Enables Clearing Of The Depth Buffer
+  glDepthFunc (GL_LESS);	// The Type Of Depth Test To Do
+  glEnable (GL_DEPTH_TEST);	// Enables Depth Testing
+  glShadeModel (GL_SMOOTH);	// Enables Smooth Color Shading
 
-	/* Start Event Processing Engine */
-	glutMainLoop ();
+  glMatrixMode (GL_PROJECTION);
+  glLoadIdentity ();
+  gluPerspective (45.0f, (GLfloat)WIDTH / (GLfloat)HEIGHT, 0.1f, 100.0f);
 
-	return 0;
+  glMatrixMode (GL_MODELVIEW);
+  glLoadIdentity ();
+  glTranslated (0.0, -0.5, -4.0);
+
+  draw_scene ();
+
+  /* Start Event Processing Engine */
+  glutMainLoop ();
+
+  return 0;
 }
